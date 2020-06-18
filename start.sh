666b484af8694359055d02bf5e360378dc3eeb1d
root <ncamposleal@gmail.com>
2020-06-18 18:20:27 -0400

    last

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
 sudo su
 
-composer install 
+#clonar repositorio
+git clone https://github.com/ncamposleal/gianmonkeyrobottestqueue.git monkey-test-back
 
-apt install php-mysql
+composer update 
+
 php artisan migrate
 
 php artisan l5-swagger:generate
 chmod -R 777 .
+
 # Inicia los Jobs Creados
 php artisan queue:work --queue=high,low
