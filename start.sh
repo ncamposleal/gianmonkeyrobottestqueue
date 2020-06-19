08f6533e928c1178c01fd2c63e97a688c4c7117f
Nicolas Campos <ncamposleal@gmail.com>
2020-06-18 20:14:10 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 #Instala dependencias
 composer update 
 
-##Crea tablas y usuario (Se debe ya haber seteado en el .env la configuracion a la bd)
+#Crea tablas y usuario (Se debe ya haber seteado en el .env la configuracion a la bd)
 php artisan migrate
 
 #Permisos para la documentacion de swagger
 chmod -R 777 .
 
-# Este proceso toma los Jobs enviados a la query y los procesa segun orden de prioridad del job  
+#Este proceso toma los Jobs enviados a la query y los procesa segun orden de prioridad del job  
 php artisan queue:work --queue=high,low
 echo "running queue job"
